00fe100ddcabc076151ee491d7fa233fd13b5716
Roberto Amorim <rjamorim@yahoo.com>
2017-10-21 12:20:54 -0200

    Fixed link appearance

--- index.php
+++ index.php
@@ -68,9 +68,9 @@
 <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/">
 <img alt="Creative Commons License" style="border-width:0" 
 src="https://i.creativecommons.org/l/by-sa/4.0/88x31.png" /></a>
-<br />All text in this website is licensed under a<a rel="license" 
-href="http://creativecommons.org/licenses/by-sa/4.0/">Creative Commons 
-Attribution-ShareAlike 4.0 International License</a>.
+<br />All text in this website is licensed under a <a style="color: white" 
+rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/">Creative 
+Commons Attribution-ShareAlike 4.0 International License</a>
 
 <!-- /Creative Commons License -->
 
