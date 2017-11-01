3cf9121b6403b75dfd3b8ba6c4a02838c55c61e6
Roberto Amorim <rjamorim@yahoo.com>
2017-11-01 11:43:05 -0200

    Adding a page for Emagic Zap lossless compressor

---
+++ zap.php
@@ -0,0 +1,154 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+
+<HTML>
+<HEAD>
+<TITLE>ReallyRareWares - Emagic Zap</TITLE>
+
+</HEAD>
+<BODY bgcolor="#FFFFFF">
+
+<table border="0" cellpadding="2" cellspacing="0" width="100%">
+  <!-- Titulo -->
+  <tr>
+  <td>
+  <table border="0" cellpadding="0" cellspacing="0" width="100%">
+    <tr><td align="center"><img src="img/title.gif"></td>
+    </tr>
+  </table>
+  </td>
+  </tr>
+
+  <tr>
+  <td bgcolor="#6699CC"><img height=15 width=1 src="dot.gif" alt="null"></td>
+  </tr>
+
+  <tr>
+  <td>
+  <table border="0" cellpadding="0" cellspacing="0" width="100%">
+    <tr valign="top">
+    <!-- Menu -->
+    <td width=120 nowrap>
+    <table border="0" cellpadding="0" cellspacing="0">
+      <tr valign="top">
+      <td bgcolor="#999999" width=119 align="center" nowrap>
+      <font size="1" face="Tahoma">
+      <table border="0" cellpadding="0" cellspacing="0" width="120">
+    
+    </td>
+      </table>
+      <table border="0" cellpadding="0" cellspacing="0" width="120">
+    <tr><td bgcolor="#669999"><font face="Tahoma" size="2">
+      <body link="#999999" vlink="#999999" alink="#999999">
+        <center><img src="img/menu.gif"></center>
+    </font></td></tr>
+      </table>
+<br>
+    <?php include("menu.htm") ?>
+
+
+      <table border="0" cellpadding="0" cellspacing="0" width="120">
+    <tr><td></td></tr>
+    <tr></tr>
+    <tr><td></td></tr>
+    
+    
+      </table>
+<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+      <table border="0" cellpadding="0" cellspacing="0" width="120">
+    <tr><td height=350><img width=1 height=100 src="dot.gif"></td></tr>
+      </table>
+      </font>
+      </td>
+     
+      </tr>
+    </table></td>
+    <!-- Corpo principal -->
+    <td width="100%">
+    <table border="0" cellpadding="0" cellspacing="0" width="100%">
+      <tr><td>
+        <table border="0" cellpadding="0" cellspacing="0" width="100%">
+      <tr>
+      <td width="100%" align="right">
+      </td>
+      </tr>
+    </table></td></tr>
+      <tr><td>
+        <table border="0" cellpadding="10" cellspacing="0" width="100%">
+      <tr><td width="450"></td>
+        <td width="20%"></td></tr>
+      <tr><td>
+        <font face="Geneva,Verdana,Helvetica,Arial">
+
+        <!-- Corpo agora -->
+        
+        <table border="0" cellpadding="2" cellspacing="0" width="20">
+          <tr bgcolor="669999"><td width="20"><img width=20 height=1 src="dot.gif"></td>
+          <td><font face="Tahoma" size="4" color="black"><img src="img/programs2.gif"></font></td>
+          <td width="20"><img width=20 height=1 src="dot.gif"></td></tr>
+        </table>
+        </a>
+        <body link="#000000" vlink="#000000" alink="#000000">
+        <font face="Verdana" size=2 color="6699CC">
+          <br><div align="justify"><b>Emagic Zap</b></div><p></font>
+        <font face="Tahoma" size=2 color="black"><blockquote>
+        <a href="screens/zap.png" target=outside><img src="screens/zap.png" 
+        alt="Zap demo. So ugly!" align=right height="70%"></a>
+        Zap is a lossless compressor created by famous German music software and 
+        hardware company Emagic, makers of the Logic sequencer.<br><br>
+        
+        The compressor is a demo version and is only available for the Mac OS 
+        platform, but there are freeware decompressors for Windows and Mac OS
+        (actually, adware because they continuously scroll adverts for Emagic 
+        products). I could not get to use the decompressors in either Windows or
+        Mac OS. I thought they were drag-and-drop applications, but that does not
+        seem to be the case.<br><br>
+        
+        One interesting feature of Zap is that it can create self extracting 
+        archives. Unfortunately, that functionality is not available in the demo
+        version.<br><br>
+        
+        In July 2002 Emagic was bought by Apple Computer and Zap was discontinued
+        (maybe it lives on in <a href="https://en.wikipedia.org/wiki/Apple_Lossless"
+        target=outside>ALAC</a>?). You can still see a mirror of the web page at the <a
+        href="https://web.archive.org/web/20000420025145/http://www.emagic.de/english/products/software/zap.html"
+        target=outside>Internet Archive</a>.<br>
+
+        <br>
+        <br>
+        Date: 1999-06-03<br>
+        Version: 1.5<br>
+        Interface: Graphical<br>
+        Platform: Mac OS Classic<br>
+        Download: <a href="files/lossless/zap15dem.sit">zap15dem.sit</a> - 717Kb<br>
+        <br>
+        Date: 1997-03-13<br>
+        Version: 1.0.1<br>
+        Interface: Graphical<br>
+        Platform: Win32<br>
+        Download: <a href="files/lossless/unzapwin.zip">unzapwin.zip</a> - 157Kb<br>
+        <br>
+        Date: 1997-05-07<br>
+        Version: 1.1.4<br>
+        Interface: Graphical<br>
+        Platform: Mac OS Classic<br>
+        Download: <a href="files/lossless/dezap.sit">dezap.sit</a> - 146Kb<br>
+
+        </blockquote></font>
+
+        <br><br>
+
+        </font>
+      </td></tr>
+    </table>
+      </td></tr>
+    </table>
+    </td>
+    </tr>
+  </table></td>
+  </tr>
+</table> 
+ 
+<?php include("footer.htm") ?>
+
+</body>
+</html>
