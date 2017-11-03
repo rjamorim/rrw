21b20cea64cc7d24ad7b01d89c2728a8fe625223
Roberto Amorim <rjamorim@yahoo.com>
2017-11-03 16:08:47 -0200

    Adding page for Sound Limit MP3 encoder

---
+++ soundlimit.php
@@ -0,0 +1,142 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+
+<HTML>
+<HEAD>
+<TITLE>ReallyRareWares - Sound Limit MP1/2/3 encoder</TITLE>
+
+</HEAD>
+<BODY bgcolor="#FFFFFF">
+
+<table border="0" cellpadding="2" cellspacing="0" width="100%">
+  <!-- Titulo -->
+  <tr>
+  <td>
+  <table border="0" cellpadding="0" cellspacing="0" width="100%">
+    <tr><td align="center"><img src="img/title.gif"></td>
+    </tr>
+  </table>
+  </td>
+  </tr>
+
+  <tr>
+  <td bgcolor="#6699CC"><img height=15 width=1 src="dot.gif" alt="null"></td>
+  </tr>
+
+  <tr>
+  <td>
+  <table border="0" cellpadding="0" cellspacing="0" width="100%">
+    <tr valign="top">
+    <!-- Menu -->
+    <td width=120 nowrap>
+    <table border="0" cellpadding="0" cellspacing="0">
+      <tr valign="top">
+      <td bgcolor="#999999" width=119 align="center" nowrap>
+      <font size="1" face="Tahoma">
+      <table border="0" cellpadding="0" cellspacing="0" width="120">
+    
+    </td>
+      </table>
+      <table border="0" cellpadding="0" cellspacing="0" width="120">
+    <tr><td bgcolor="#669999"><font face="Tahoma" size="2">
+      <body link="#999999" vlink="#999999" alink="#999999">
+        <center><img src="img/menu.gif"></center>
+    </font></td></tr>
+      </table>
+<br>
+    <?php include("menu.htm") ?>
+
+
+      <table border="0" cellpadding="0" cellspacing="0" width="120">
+    <tr><td></td></tr>
+    <tr></tr>
+    <tr><td></td></tr>
+    
+    
+      </table>
+<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+      <table border="0" cellpadding="0" cellspacing="0" width="120">
+    <tr><td height=250><img width=1 height=100 src="dot.gif"></td></tr>
+      </table>
+      </font>
+      </td>
+     
+      </tr>
+    </table></td>
+    <!-- Corpo principal -->
+    <td width="100%">
+    <table border="0" cellpadding="0" cellspacing="0" width="100%">
+      <tr><td>
+        <table border="0" cellpadding="0" cellspacing="0" width="100%">
+      <tr>
+      <td width="100%" align="right">
+      </td>
+      </tr>
+    </table></td></tr>
+      <tr><td>
+        <table border="0" cellpadding="10" cellspacing="0" width="100%">
+      <tr><td width="450"></td>
+        <td width="20%"></td></tr>
+      <tr><td>
+        <font face="Geneva,Verdana,Helvetica,Arial">
+
+        <!-- Corpo agora -->
+        
+        <table border="0" cellpadding="2" cellspacing="0" width="20">
+          <tr bgcolor="669999"><td width="20"><img width=20 height=1 src="dot.gif"></td>
+          <td><font face="Tahoma" size="4" color="black"><img src="img/programs2.gif"></font></td>
+          <td width="20"><img width=20 height=1 src="dot.gif"></td></tr>
+        </table>
+        </a>
+        <body link="#000000" vlink="#000000" alink="#000000">
+        <font face="Verdana" size=2 color="6699CC">
+          <br><div align="justify"><b>Sound Limit MP1/2/3 encoder</b></div><p></font>
+        <font face="Tahoma" size=2 color="black"><blockquote>
+        <a href="screens/soundlimit.png" target=outside><img src="screens/soundlimit.png" 
+        alt="Sound Limit 2.0" align=right height="70%"></a>
+        Sound Limit was a shareware MP3 encoder created in Romania by Eastern
+        Digital.<br><br>
+        
+        Considering its age, speed and user comments about bad quality, it seems to 
+        be little more than a dist10 compile with <a href="screens/soundlimit1.png"
+        target=outside>quite</a> <a href="screens/soundlimit2.png" target=outside>
+        ugly</a> <a href="screens/soundlimit3.png" target=outside>interface</a>.
+        Which is all the more amazing considering registration cost 30 dollars.<br><br>
+        
+        The demo version hosted here is only valid for 30 days after installation.
+        A mirror of the web page is available <a 
+        href="https://web.archive.org/web/20000305214044/http://2edc.com:80/products/slimit/index.html"
+        target=outside>here</a>.<br>
+
+        <br>
+        <br>
+        Date: 1999-07-02<br>
+        Version: 2.51<br>
+        Interface: Graphical<br>
+        Platform: Win32<br>
+        Download: <a href="files/mpeg/slshw25r.zip">slshw25r.zip</a> - 1.143Kb<br>
+        <br>
+        Date: 1998-09-21<br>
+        Version: 2.0<br>
+        Interface: Graphical<br>
+        Platform: Win32<br>
+        Download: <a href="files/mpeg/sl20shw.zip">sl20shw.zip</a> - 1.218Kb<br>
+
+        </blockquote></font>
+
+        <br><br>
+
+        </font>
+      </td></tr>
+    </table>
+      </td></tr>
+    </table>
+    </td>
+    </tr>
+  </table></td>
+  </tr>
+</table> 
+ 
+<?php include("footer.htm") ?>
+
+</body>
+</html>
