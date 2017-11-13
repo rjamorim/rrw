bed2d270c37e6ad1c454be56de32c3d1fa8086f2
Roberto Amorim <rjamorim@yahoo.com>
2017-11-13 13:40:55 -0200

    Added the AUPEC command line encoder

--- aupec.php
+++ aupec.php
@@ -113,6 +113,12 @@
         Platform: Win32<br>
         Download: <a href="files/AUPEC_Enc.exe">AUPEC_Enc.exe</a> - 632Kb<br>
         Winamp Plugin: <a href="files/AUPEC_Plugin.exe">AUPEC_Plugin.exe</a> - 49Kb<br>
+        <br>
+        Date: 2000<br>
+        Version: g2<br>
+        Interface: Command line<br>
+        Platform: Win32<br>
+        Download: <a href="files/aupec-cli.zip">aupec-cli.zip</a> - 84Kb<br>
 
         </blockquote></font>
 
