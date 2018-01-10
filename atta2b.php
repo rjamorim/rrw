b11ebe825bcf05c3279e3bdd3c99d7ede5388231
Roberto Amorim <rjamorim@yahoo.com>
2018-01-10 10:59:54 -0200

    Adding page for AT&T a2b music

---
+++ atta2b.php
@@ -0,0 +1,151 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+
+<HTML>
+<HEAD>
+<TITLE>ReallyRareWares - AT&amp;T A2B Music</TITLE>
+
+</HEAD>
+<BODY bgcolor="#FFFFFF">
+
+<table border="0" cellpadding="2" cellspacing="0" width="100%">
+  <!-- Titulo -->
+  <tr>
+  <td>
+  <table border="0" cellpadding="0" cellspacing="0" width="100%">
+    <tr><td align="center"><img src="img/title.gif"></td>
+    </tr>
+  </table>
+  </td>
+  </tr>
+
+  <tr>
+  <td bgcolor="#6699CC"><img height=15 width=1 src="dot.gif" alt="null"></td>
+  </tr>
+
+  <tr>
+  <td>
+  <table border="0" cellpadding="0" cellspacing="0" width="100%">
+    <tr valign="top">
+    <!-- Menu -->
+    <td width=120 nowrap>
+    <table border="0" cellpadding="0" cellspacing="0">
+      <tr valign="top">
+      <td bgcolor="#999999" width=119 align="center" nowrap>
+      <font size="1" face="Tahoma">
+      <table border="0" cellpadding="0" cellspacing="0" width="120">
+    
+    </td>
+      </table>
+      <table border="0" cellpadding="0" cellspacing="0" width="120">
+    <tr><td bgcolor="#669999"><font face="Tahoma" size="2">
+      <body link="#999999" vlink="#999999" alink="#999999">
+        <center><img src="img/menu.gif"></center>
+    </font></td></tr>
+      </table>
+<br>
+    <?php include("menu.htm") ?>
+
+
+      <table border="0" cellpadding="0" cellspacing="0" width="120">
+    <tr><td></td></tr>
+    <tr></tr>
+    <tr><td></td></tr>
+    
+    
+      </table>
+<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+      <table border="0" cellpadding="0" cellspacing="0" width="120">
+    <tr><td height=350><img width=1 height=100 src="dot.gif"></td></tr>
+      </table>
+      </font>
+      </td>
+     
+      </tr>
+    </table></td>
+    <!-- Corpo principal -->
+    <td width="100%">
+    <table border="0" cellpadding="0" cellspacing="0" width="100%">
+      <tr><td>
+        <table border="0" cellpadding="0" cellspacing="0" width="100%">
+      <tr>
+      <td width="100%" align="right">
+      </td>
+      </tr>
+    </table></td></tr>
+      <tr><td>
+        <table border="0" cellpadding="10" cellspacing="0" width="100%">
+      <tr><td width="450"></td>
+        <td width="20%"></td></tr>
+      <tr><td>
+        <font face="Geneva,Verdana,Helvetica,Arial">
+
+        <!-- Corpo agora -->
+        
+        <table border="0" cellpadding="2" cellspacing="0" width="20">
+          <tr bgcolor="669999"><td width="20"><img width=20 height=1 src="dot.gif"></td>
+          <td><font face="Tahoma" size="4" color="black"><img src="img/programs2.gif"></font></td>
+          <td width="20"><img width=20 height=1 src="dot.gif"></td></tr>
+        </table>
+        </a>
+        <body link="#000000" vlink="#000000" alink="#000000">
+        <font face="Verdana" size=2 color="6699CC">
+          <br><div align="justify"><b>AT&amp;T a2b music</b></div><p></font>
+        <font face="Tahoma" size=2 color="black"><blockquote>
+        <a href="screens/a2bmusic.png" target=outside><img src="screens/a2bmusic.png" 
+        alt="Indecence" align=right height="70%"></a>
+        a2b music was an attempt at creating a digital music delivery system by 
+        telecommunications giant AT&amp;T, fresh from the recording industry scare
+        caused by the sudden popularity of the MP3 format.<br><br>
+        
+        The system was launched to much fanfare in the late nineties, touting wide
+        industry support and much better quality than alternatives (because it was
+        using the AAC format, instead of the more common MP3). Of course, the sold
+        tracks were heavily protected with DRM schemes.<br><br>
+        
+        To add insult to injury, the software requires that you install RealPlayer
+        to enable streaming (to see my lenghty rant about Real crapware, read the
+        <a href="realjuke.php">RealJukebox</a> entry).<br><br>
+        
+        a2b folded rather spectaculously before even becoming relevant, when the 
+        majority of its employees decided to walk out <a 
+        href="http://www.nytimes.com/1999/05/18/business/at-t-digital-music-unit-loses-more-than-half-of-its-workers.html"
+        target=outside>and join a competitor</a> less than 2 years after it was
+        founded. For that reason, the player I am hosting here is rather useless 
+        now (unless you want to stare at an interface so ugly, it borders on the
+        indecent).<br><br>
+        
+        Talking about cutting-edge design, make sure to check this <a 
+        href="https://web.archive.org/web/19990208011048/http://www.a2bmusic.com/"
+        target=outside>archive</a> of the a2b music website, for some real <i>eye
+        candy</i>.<br><br>
+        
+        <i>Big thanks to Zachary Jelesoff for sending me the a2b music player and
+        introducing me to the story of AT&amp;T a2b.</i><br>
+
+        <br>
+        <br>
+        Date: 1999-04-09<br>
+        Version: 2.0<br>
+        Interface: Graphical<br>
+        Platform: Win32<br>
+        Download: <a href="files/players/setup_a2b_200.exe">setup_a2b_200.exe</a> - 2.559Kb<br>
+
+        </blockquote></font>
+
+        <br><br>
+
+        </font>
+      </td></tr>
+    </table>
+      </td></tr>
+    </table>
+    </td>
+    </tr>
+  </table></td>
+  </tr>
+</table> 
+ 
+<?php include("footer.htm") ?>
+
+</body>
+</html>
