bb64a16fde3b1e3b7f6617122585f5e8320f4c6b
Roberto Amorim <rjamorim@yahoo.com>
2018-01-11 14:10:25 -0200

    Visual improvements

--- apollo.php
+++ apollo.php
@@ -53,9 +53,9 @@
     
     
       </table>
-<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+<br><br><br><br><br><br><br><br><br><br><br><br><br><br>
       <table border="0" cellpadding="0" cellspacing="0" width="120">
-    <tr><td height=1250><img width=1 height=100 src="dot.gif"></td></tr>
+    <tr><td height=1300><img width=1 height=100 src="dot.gif"></td></tr>
       </table>
       </font>
       </td>
@@ -92,7 +92,7 @@
           <br><div align="justify"><b>Apollo player</b></div><p></font>
         <font face="Tahoma" size=2 color="black"><blockquote>
         <a href="screens/apollo.png" target=outside><img src="screens/apollo.png" 
-        alt="Apollo 37zz" align=right height="45%"></a>
+        alt="Apollo 37zz" align=right height="42%"></a>
         With the slogan "Sound quality is not skinnable", Apollo is an audio player with 
         strong focus on quality - its MP3 decoder is famously one of the most mathematically
         accurate ones, if not the most accurate.<br><br>
