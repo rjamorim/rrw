c3884fdb1e9baa20e0e8585776bca857f5d9fdba
Roberto Amorim <rjamorim@yahoo.com>
2018-01-15 19:44:47 -0200

    Adding Ultimate Encoder page

---
+++ ultimateenc.php
@@ -0,0 +1,144 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+
+<HTML>
+<HEAD>
+<TITLE>ReallyRareWares - Ultimate Encoder</TITLE>
+
+</HEAD>
+<BODY bgcolor="#FFFFFF">
+
+<table border="0" cellpadding="2" cellspacing="0" width="100%">
+  <!-- Titulo -->
+  <tr>
+  <td>
+  <table border="0" cellpadding="0" cellspacing="0" width="100%">
+    <tr><td align="center"><img src="img/title.gif"></td>
+    </tr>
+  </table>
+  </td>
+  </tr>
+
+  <tr>
+  <td bgcolor="#6699CC"><img height=15 width=1 src="dot.gif" alt="null"></td>
+  </tr>
+
+  <tr>
+  <td>
+  <table border="0" cellpadding="0" cellspacing="0" width="100%">
+    <tr valign="top">
+    <!-- Menu -->
+    <td width=120 nowrap>
+    <table border="0" cellpadding="0" cellspacing="0">
+      <tr valign="top">
+      <td bgcolor="#999999" width=119 align="center" nowrap>
+      <font size="1" face="Tahoma">
+      <table border="0" cellpadding="0" cellspacing="0" width="120">
+    
+    </td>
+      </table>
+      <table border="0" cellpadding="0" cellspacing="0" width="120">
+    <tr><td bgcolor="#669999"><font face="Tahoma" size="2">
+      <body link="#999999" vlink="#999999" alink="#999999">
+        <center><img src="img/menu.gif"></center>
+    </font></td></tr>
+      </table>
+<br>
+    <?php include("menu.htm") ?>
+
+
+      <table border="0" cellpadding="0" cellspacing="0" width="120">
+    <tr><td></td></tr>
+    <tr></tr>
+    <tr><td></td></tr>
+    
+    
+      </table>
+<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+      <table border="0" cellpadding="0" cellspacing="0" width="120">
+    <tr><td height=250><img width=1 height=100 src="dot.gif"></td></tr>
+      </table>
+      </font>
+      </td>
+     
+      </tr>
+    </table></td>
+    <!-- Corpo principal -->
+    <td width="100%">
+    <table border="0" cellpadding="0" cellspacing="0" width="100%">
+      <tr><td>
+        <table border="0" cellpadding="0" cellspacing="0" width="100%">
+      <tr>
+      <td width="100%" align="right">
+      </td>
+      </tr>
+    </table></td></tr>
+      <tr><td>
+        <table border="0" cellpadding="10" cellspacing="0" width="100%">
+      <tr><td width="450"></td>
+        <td width="20%"></td></tr>
+      <tr><td>
+        <font face="Geneva,Verdana,Helvetica,Arial">
+
+        <!-- Corpo agora -->
+        
+        <table border="0" cellpadding="2" cellspacing="0" width="20">
+          <tr bgcolor="669999"><td width="20"><img width=20 height=1 src="dot.gif"></td>
+          <td><font face="Tahoma" size="4" color="black"><img src="img/programs2.gif"></font></td>
+          <td width="20"><img width=20 height=1 src="dot.gif"></td></tr>
+        </table>
+        </a>
+        <body link="#000000" vlink="#000000" alink="#000000">
+        <font face="Verdana" size=2 color="6699CC">
+          <br><div align="justify"><b> Ultimate Encoder </b></div><p></font>
+        <font face="Tahoma" size=2 color="black"><blockquote>
+        <a href="screens/ultimateenc.png" target=outside><img src="screens/ultimateenc.png" 
+        alt="Ultimate Encoder 1.5" align=right height="70%"></a>
+        OK, so here we have an MP1/2/3 encoder. Commercial (registration cost 49
+        dollars). Looks and sounds like little more than a dist10 compile. <a 
+        href="screens/ultimateenc1.png" target=outside>Horrible</a> <a 
+        href="screens/ultimateenc2.png" target=outside>wizard-based</a> <a 
+        href="screens/ultimateenc3.png" target=outside>interface</a>. Can also 
+        decode MP1/2/3 and rip CDs. Comes from Romania.<br><br>
+    
+        Sounds familiar?<br><br>
+        
+        I honestly do not know the stories behind Ultimate Encoder and <a 
+        href="soundlimit.php">Sound Limit</a>, but something fishy is definitely 
+        going on. Please compare the screenshots available above and the ones at 
+        SoundLimit's page. Even some of the (ugly, unnecessary) textures are the 
+        same!<br><br>
+        
+        If you know any story, please get in <a href="about.php">touch</a>!<br><br>
+        
+        The demo version hosted here is only valid for 30 days after installation.
+        A mirror of the web page is available <a 
+        href="https://web.archive.org/web/20000304072224/http://www.usro.net:80/products/uencoder/index.htm"
+        target=outside>here</a>.<br>
+
+        <br>
+        <br>
+        Date: 1999-08-19<br>
+        Version: 1.5<br>
+        Interface: Graphical<br>
+        Platform: Win32<br>
+        Download: <a href="files/mpeg/ueshw15a.zip">ueshw15a.zip</a> - 1.470Kb<br>
+
+        </blockquote></font>
+
+        <br><br>
+
+        </font>
+      </td></tr>
+    </table>
+      </td></tr>
+    </table>
+    </td>
+    </tr>
+  </table></td>
+  </tr>
+</table> 
+ 
+<?php include("footer.htm") ?>
+
+</body>
+</html>
