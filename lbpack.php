e8b295c3002cfcf0c303e4857d736bbcea5f21c1
Roberto Amorim <rjamorim@yahoo.com>
2018-02-16 17:17:02 -0200

    Fix grammar error

--- lbpack.php
+++ lbpack.php
@@ -94,24 +94,25 @@
         <font face="Tahoma" size=2 color="black"><blockquote>
         <a href="screens/lbpack.png" target=outside><img src="screens/lbpack.png" 
         alt="LBPack player" align=right height="80%"></a>
-        LBPack (Low Bitrate Packer and Player) was created around 1997 by Dmitry Shmunk, an 
-        engineer working at the Budker Institute of Nuclear Physics in Russia. He later went
-        on to work at QDesign in Canada, and LBpack became the <a href="qdmc.php">QDesign 
-        Music Codec</a>.<br><br>
+        LBPack (Low Bitrate Packer and Player) was created around 1997 by Dmitry Shmunk, 
+        an engineer working at the Budker Institute of Nuclear Physics in Russia. He 
+        later went on to work at QDesign in Canada, and LBpack became the <a 
+        href="qdmc.php">QDesign Music Codec</a>.<br><br>
         
-        It is optimized for extremely low bitrates (the default bitrate is 22kbps!). And 
-        indeed it delivers acceptable quality at these bitrates. But the codec is so 
-        aggressive that even at higher bitrates it can not achieve transparency.<br><br>
+        It is optimized for extremely low bitrates (the default bitrate is 22kbps!). 
+        And indeed it delivers acceptable quality at these bitrates. But the codec 
+        is so aggressive that even at higher bitrates it can not achieve transparency.
+        <br><br>
         
         It suffers from many of the problems QDesign suffers: distortions when encoding 
         voice, annoying artifacts, etc.<br><br>
         
-        The official page has been taken offline after the codec was bought by QDesign, so
-        nowadays the only information about this codec is available at the readme and faq 
-        inside the download package.<br><br>
+        The official page was taken offline after the codec was bought by QDesign, 
+        so nowadays the only information about this codec is available at the readme 
+        and FAQ inside the download package.<br><br>
         
-        <i>Lots of thanks to Geert from Belgium for providing this codec and enlightening me
-        on its history and its relation to QDesign.</i><br>
+        <i>Lots of thanks to Geert from Belgium for providing this codec and 
+        enlightening me on its history and its relation to QDesign.</i><br>
         
         <br>
         <br>
