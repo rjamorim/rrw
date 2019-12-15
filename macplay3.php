f8bc2ed1ec2e1c5fe70112f93fb442e7708da903
Roberto Amorim <rjamorim@yahoo.com>
2019-12-14 22:01:42 -0300

    Improving the text

--- macplay3.php
+++ macplay3.php
@@ -94,15 +94,14 @@
         <font face="Tahoma" size=2 color="black"><blockquote>
         <a href="screens/macplay3v14.png" target=outside><img src="screens/macplay3v14.png" 
         alt="MacPlay3 v1.4" align=right></a>
-        MacPlay3 was FhG's port of the <a href="winplay3.php">WinPlay3</a> MP3 player for 
-        Mac OS Classic. It seems to have the same (few) features of its Windows counterpart.
-        <br><br>
+        MacPlay3 was Fraunhofer's port of the <a href="winplay3.php">WinPlay3</a> MP3 player 
+        for Mac OS Classic. It has the same (few) features of its Windows counterpart.<br><br>
         
         The alpha 0.99 version works on the M68K platform, but it is only useful for 
         decoding, since the 68k CPU is not powerful enough to playback MP3 in real time. 
         Besides, it is a demo version with no interface (all commands are done through
         the menu bar, similar to Winamp 0.2) and playback is limited to 20 seconds
-        (but not decoding).<br><br>
+        (decoding to AIFF is not limited).<br><br>
         
         Version 1.4b2 was the last version released by FhG and only works on 
         PowerPC machines, but it has an actual interface and no limitations.<br><br>
