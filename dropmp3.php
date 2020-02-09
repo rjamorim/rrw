2bc1d9bf56f67b4b94b242675fce4345843a689f
Roberto Amorim <rjamorim@yahoo.com>
2020-02-09 14:03:55 -0300

    Added the DropMP3 page

---
+++ dropmp3.php
@@ -0,0 +1,148 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+
+<HTML>
+<HEAD>
+<TITLE>ReallyRareWares - DropMP3 encoder for MacOS</TITLE>
+
+</HEAD>
+<BODY bgcolor="#FFFFFF">
+
+<table border="0" cellpadding="2" cellspacing="0" width="100%">
+  <!-- Titulo -->
+  <tr>
+  <td>
+  <table border="0" cellpadding="0" cellspacing="0" width="100%">
+    <tr><td align="center"><img src="img/title.gif"></td>
+    </tr>
+  </table>
+  </td>
+  </tr>
+
+  <tr>
+  <td bgcolor="#6699CC"><img height=15 width=1 src="dot.gif" alt="null"></td>
+  </tr>
+
+  <tr>
+  <td>
+  <table border="0" cellpadding="0" cellspacing="0" width="100%">
+    <tr valign="top">
+    <!-- Menu -->
+    <td width=120 nowrap>
+    <table border="0" cellpadding="0" cellspacing="0">
+      <tr valign="top">
+      <td bgcolor="#999999" width=119 align="center" nowrap>
+      <font size="1" face="Tahoma">
+      <table border="0" cellpadding="0" cellspacing="0" width="120">
+    
+    </td>
+      </table>
+      <table border="0" cellpadding="0" cellspacing="0" width="120">
+    <tr><td bgcolor="#669999"><font face="Tahoma" size="2">
+      <body link="#999999" vlink="#999999" alink="#999999">
+        <center><img src="img/menu.gif"></center>
+    </font></td></tr>
+      </table>
+<br>
+    <?php include("menu.htm") ?>
+
+
+      <table border="0" cellpadding="0" cellspacing="0" width="120">
+    <tr><td></td></tr>
+    <tr></tr>
+    <tr><td></td></tr>
+    
+    
+      </table>
+<br><br><br><br><br><br><br><br><br><br><br><br>
+      <table border="0" cellpadding="0" cellspacing="0" width="120">
+    <tr><td height=350><img width=1 height=100 src="dot.gif"></td></tr>
+      </table>
+      </font>
+      </td>
+     
+      </tr>
+    </table></td>
+    <!-- Corpo principal -->
+    <td width="100%">
+    <table border="0" cellpadding="0" cellspacing="0" width="100%">
+      <tr><td>
+        <table border="0" cellpadding="0" cellspacing="0" width="100%">
+      <tr>
+      <td width="100%" align="right">
+      </td>
+      </tr>
+    </table></td></tr>
+      <tr><td>
+        <table border="0" cellpadding="10" cellspacing="0" width="100%">
+      <tr><td width="450"></td>
+        <td width="20%"></td></tr>
+      <tr><td>
+        <font face="Geneva,Verdana,Helvetica,Arial">
+
+        <!-- Corpo agora -->
+        
+        <table border="0" cellpadding="2" cellspacing="0" width="20">
+          <tr bgcolor="669999"><td width="20"><img width=20 height=1 src="dot.gif"></td>
+          <td><font face="Tahoma" size="4" color="black"><img src="img/programs2.gif"></font></td>
+          <td width="20"><img width=20 height=1 src="dot.gif"></td></tr>
+        </table>
+        </a>
+        <body link="#000000" vlink="#000000" alink="#000000">
+        <font face="Verdana" size=2 color="6699CC">
+          <br><div align="justify"><b> DropMP3 encoder for MacOS </b></div><p></font>
+        <font face="Tahoma" size=2 color="black"><blockquote>
+        <a href="screens/DropMP3.png" target=outside><img src="screens/DropMP3.png" 
+        alt="DropMP3 Encoder for MacOS" align=right height=250></a>
+        DropMP3 is, to the best of my knowledge, the only MP3 encoder for Mac OS
+        Classic using <a href=lame.php>LAME</a>. Since MacOS Classic never featured
+        a command line interface, native command line applications had to work 
+        around this limitation - either by placing the command line interface 
+        inside a special window (as in <a href=blade.php>BladeMac</a>), or by using
+        a GUI to interface with the encoding library, as is the case here.<br><br>
+        
+        It was created around 2000 by Philippe Laval. It has all the oprions you 
+        would expect from a LAME frontend: choice of bitrate (CBR and VBR), stereo
+        encoding mode, lowpass flters, resampling, tagging...<br><br>
+        
+        The LAME library is distributed separately from the GUI, both files are 
+        available below. Unfortunately the latest version I could find was 3.70,
+        it seems the author never compiled a newer version.<br><br>
+        
+        The web site is surprisingly still available, <a 
+        href="http://philippe.laval.free.fr/DropMP3/US/index.php" 
+        target=outside>here</a>.<br>
+
+        <br>
+        <br>
+        Date: 2000-04-15<br>
+        Version: 1.0.6<br>
+        Interface: Graphical<br>
+        Platform: MacOS Classic PPC<br>
+        Download: <a href="files/mpeg/DropMP3_1.0.6.sea.hqx">DropMP3_1.0.6.sea.hqx</a> - 212kB<br>
+        <br>
+        Date: 2000-04-19<br>
+        Version: 3.70<br>
+        Interface: Library<br>
+        Platform: MacOS Classic PPC<br>
+        Download: <a href="files/mpeg/Lame_SharedLib_ALTIVEC.sea.hqx">Lame_SharedLib_ALTIVEC.sea.hqx</a> - 190kB<br>
+
+        </blockquote></font>
+
+        <br><br>
+
+        </font>
+      </td></tr>
+    </table>
+      </td></tr>
+    </table>
+    </td>
+    </tr>
+  </table></td>
+  </tr>
+</table> 
+ 
+<?php include("footer.htm") ?>
+
+</body>
+</html>
