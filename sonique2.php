96091fa5c7aaadd2e2b1a8ef37932a01a55e58a4
Roberto Amorim <rjamorim@yahoo.com>
2020-07-13 20:43:07 -0300

    Removed some dead links, replaced others with archive.org

--- sonique2.php
+++ sonique2.php
@@ -94,45 +94,50 @@
         </font>
         <font face="Tahoma" size=2 color="black"><blockquote>
         <a href="screens/sonique2.png" target=outside><img src="screens/sonique2.png" 
-        alt="The interface of discord" align=right height=240></a>
-        <p><i><b>Part of the following story is flawed. I'm leaving it here for humour
-        purposes. For a more truthful depiction of what really happened, read the bottom of
-        the page.</b></i></p>
+        alt="The interface of discord" align=right height=280></a>
+        <p><i><b>Part of the following story is flawed. I'm leaving it here for 
+        humour purposes. For a more truthful depiction of what really happened, 
+        read the bottom of the page.</b></i></p>
         
-        Once upon a time, an alpha of Sonique 2 was leaked (note: this isn't the alpha/beta 
-        that was available for a while in the the official <a href="http://sonique.lycos.com" 
-        target=outside>Sonique</a> page. That beta - now dead too - was developed by the new 
+        Once upon a time, an alpha of Sonique 2 was leaked (note: this isn't the 
+        alpha/beta that was available for a while in the the official <a target=outside
+        href="http://web.archive.org/web/20050307093203/http://sonique.lycos.com/">
+        Sonique</a> page. That beta - now dead too - was developed by the new 
         development team. The alpha here was developed by the original team).<br><br>
         
-        I remember Sonique fans were completely hysterical about this alpha. They went to
-        great lenghts to shut down every site hosting it, supposedly out of fear Lycos would 
-        drop development because of this high treason. The real reason, of course, is that 
-        they wanted to be the only "kool boyz" to own such a rare tool and flaunter it in 
-        front of helpless newbies that badly wanted it too.<br><br>
+        I remember Sonique fans were completely hysterical about this alpha. They 
+        went to great lenghts to shut down every site hosting it, supposedly out 
+        of fear Lycos would drop development because of this high treason. The 
+        real reason, of course, is that they wanted to be the only "kool boyz" to 
+        own such a rare tool and flaunter it in front of helpless newbies that 
+        badly wanted it too.<br><br>
         
-        A good image of how things were dealt with those days at the Sonique forums can be 
-        depicted by <a href="files/players/asl.txt" target=outside>this protest article</a>
-        that was posted at one of them.<br><br>
+        A good image of how things were dealt with those days at the Sonique 
+        forums can be depicted by <a href="files/players/asl.txt" target=outside>
+        this protest article</a> that was posted at one of them.<br><br>
         
-        Unfortunately for them, a copy landed in my hands. In yet another attempt to prove
-        my theory that "once something appears on the Internet, no amount of effort can
-        take it down", I hosted it, accompanied by a very crude page, at my university 
-        server. And so <a href="https://www.rarewares.org" target=outside>RareWares</a> 
-        was born.<br><br>
+        Unfortunately for them, a copy landed in my hands. In yet another attempt 
+        to prove my theory that "once something appears on the Internet, no amount 
+        of effort can take it down", I hosted it, accompanied by a very crude 
+        page, at my university server. And so <a href="https://www.rarewares.org" 
+        target=outside>RareWares</a> was born.<br><br>
         
-        So here it is for you. The rage-inducing Sonique 2 alpha in all its unfinished 
-        glory. The interface is quite bloated, matter of factly, and functionality is
-        minimal. Maybe I am not really into eye-candy overkill... If this was the future 
-        of Sonique, it is not a surprise it completely flunked.<br><br>
+        So here it is for you. The rage-inducing Sonique 2 alpha in all its 
+        unfinished glory. The interface is quite bloated, matter of factly, and 
+        functionality is minimal. Maybe I am not really into eye-candy overkill...
+        If this was the future of Sonique, it is not a surprise it completely 
+        bombed.<br><br>
         
-        Ah, yes. As it turns out, Lycos noticed they were just wasting money with their 
-        Sonique division and decided to fire all developers. An outcry ensued, and they
-        hired two new developers to continue Sonique 2 development. Soon after, though,
-        they realized they owed nothing to a handful of squabbling nerds with too much
-        time on their hands, and finally let Sonique die a (mostly) peaceful death.<br><br>
+        Ah, yes. As it turns out, Lycos noticed they were just wasting money 
+        with their Sonique division and decided to fire all developers. An 
+        outcry ensued, and they hired two new developers to continue Sonique 2 
+        development. Soon after, though, they realized they owed nothing to a 
+        handful of squabbling nerds with too much time on their hands, and 
+        finally let Sonique die a (mostly) peaceful death.<br><br>
         
         If you are looking for old, official versions of Sonique 1, look at <a 
-        href="http://www.glop.org/sonique/" target=outside>The Museum of Sonique</a>.<br>
+        href="http://www.glop.org/sonique/" target=outside>The Museum of 
+        Sonique</a>.<br>
         
         <br>
         <br>
@@ -147,11 +152,11 @@
         <li><p>Feedback from Bits:</p></li>
         </ul>
         
-        <i><p>Andrew McCann himself stated/requested/pleaded that the leak needed to 
-        be stopped (as he was in negotiations to buy Sonique2 from Lycos), the leak 
-        could have potentially screwed things. (BTW it was leaked only accidently by 
-        Tony Million as he left it in a open dir on his private (yet publically 
-        accessible) home server @ http://www.tonymillion.com))</p>
+        <i><p>Andrew McCann himself stated/requested/pleaded that the leak needed 
+        to be stopped (as he was in negotiations to buy Sonique2 from Lycos), the 
+        leak could have potentially screwed things. (BTW it was leaked only 
+        accidently by Tony Million as he left it in a open dir on his private 
+        (yet publically accessible) home server @ http://www.tonymillion.com))</p>
         
         <p>So it was nothing to do with fanboys trying to stop the world getting 
         it.. it was merely holding back the leak until after negotiations had 
@@ -165,29 +170,30 @@
         later decided to locate more people to develop a program they never 
         actually tried to completely kill (they merely needed to cut it back)</p>
         
-        <p>Of course all said is only to the best of my knowledge (I was there the
-        hour the alpha was found on Tony's ftp, I was talking to Tony when he said 
-        it was an accident but added (in the direction to the likes of Andrew and co.) 
-        "at least someone had the balls to release it" (or something close to that))</p>
+        <p>Of course all said is only to the best of my knowledge (I was there
+        the hour the alpha was found on Tony's ftp, I was talking to Tony when 
+        he said it was an accident but added (in the direction to the likes of 
+        Andrew and co.) "at least someone had the balls to release it" (or 
+        something close to that))</p>
 
         <p>I was there when Andrew requested the leak to be stopped as soon as
-        possible as it was potentially making things much worse for Sonique (he said 
-        it via the forum, IRC and ICQ, plus his girlfriend at the time Anna did as
-        well, who BTW was the one who originally broke the month long silence about 
-        the mass layoff of Team Sonique).</p>
+        possible as it was potentially making things much worse for Sonique (he 
+        said it via the forum, IRC and ICQ, plus his girlfriend at the time Anna 
+        did as well, who BTW was the one who originally broke the month long 
+        silence about the mass layoff of Team Sonique).</p>
         
-        <p>Of course I am not Andrew or Tony or Ian and can't speak for them. But my 
-        story is about 100x closer to what really happened compared to yours (I am 
-        confident my version is rather accurate)</p>
+        <p>Of course I am not Andrew or Tony or Ian and can't speak for them. 
+        But my story is about 100x closer to what really happened compared to 
+        yours (I am confident my version is rather accurate)</p>
         
-        <p>To conclude the epic Sonique tale... Tony wrote Tuniac after he left Sonique
-        (<a href="http://www.tuniac.com" target=outside>http://www.tuniac.com</a>, or 
-        for a more recent compile go to <a href="http://www.wasteofcash.com/tuniac/"
-        target=outside>http://www.wasteofcash.com/tuniac/</a> )</p>
+        <p>To conclude the epic Sonique tale... Tony wrote Tuniac after he left 
+        Sonique (http://www.tuniac.com, or for a more recent compile go to 
+        http://www.wasteofcash.com/tuniac/)</p>
 
-        <p>Currently we are lazy, and Tony believes he is now a photographer not a programmer. 
-        Blur is MIA. Tuniac2 with an OpenGL GUI, SQL database, etc, etc. is like 10% done (and 
-        for a  month.. or two... has had no updates)</p></i>
+        <p>Currently we are lazy, and Tony believes he is now a photographer not
+        a programmer. Blur is MIA. Tuniac2 with an OpenGL GUI, SQL database, 
+        etc, etc. is like 10% done (and for a  month.. or two... has had no 
+        updates)</p></i>
 
         </blockquote></font>
 
