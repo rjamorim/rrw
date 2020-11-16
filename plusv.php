39ca2b93ac45f524a7ca28eeee07f6ce6dd39e9b
Roberto Amorim <rjamorim@yahoo.com>
2020-11-16 11:05:10 -0300

    Some needed updates

--- plusv.php
+++ plusv.php
@@ -1,4 +1,5 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
 
 <HTML>
 <HEAD>
@@ -91,28 +92,31 @@
         <font face="Verdana" size=2 color="6699CC">
           <br><div align="justify"><b>PlusV encoder</b></div><p></font>
         <font face="Tahoma" size=2 color="black"><blockquote>
-        PlusV was the open source alternative to <a href="ctmp3pro.php">MP3pro</a>. The 
-        encoder split wav files in two, the low frequencies were encoded with a standard
-        MP3 encoder (in this case, <a href="lame.php">LAME</a>) and the high frequencies
-        were very efficiently encoded into a special "+V" format.<br><br>
+        PlusV was the open source alternative to <a href="ctmp3pro.php">MP3pro
+        </a>. The encoder split wav files in two, the low frequencies were encoded
+        with a standard MP3 encoder (in this case, <a href="lame.php">LAME</a>) 
+        and the high frequencies were very efficiently encoded into a special 
+        "+V" format.<br><br>
         
-        The encoded .m3v files carried both frequency ranges, and a special decoder would 
-        decode both at playback time. A standard MP3 decoder can decode only the low
-        frequencies, seriously impacting quality.<br><br>
+        The encoded .m3v files carried both frequency ranges, and a special 
+        decoder would decode both at playback time. A standard MP3 decoder can 
+        decode only the low frequencies, seriously impacting quality.<br><br>
 
-        Besides the official PlusV tools, I am also hosting here some additional ones 
-        developed by "gameplaya15143": an interleaver to merge mp3 + pv streams, and a 
-        modified plusv.exe with fixes and extra functionality.<br><br>
+        Besides the official PlusV tools, I am also hosting here some additional
+        ones developed by "gameplaya15143": an interleaver to merge mp3 + pv 
+        streams, and a modified plusv.exe with fixes and extra functionality.
+        <br><br>
         
-        Unfortunately PlusV met with even less success than MP3pro, and ended up being 
-        quickly abandoned by its developers. You can still check their web page though,
-        using <a href="https://web.archive.org/web/20011128233927/http://www.plusv.org:80/" 
-        target=outside>archive.org</a>; and read the specification in <a 
-        href="https://www.mp3-tech.org/programmer/docs/plusv.pdf" target=outside>this</a> 
-        PDF document hosted by Gabriel Bouvigne.<br><br>
+        Unfortunately PlusV met with even less success than MP3pro, and ended up 
+        being quickly abandoned by its developers. You can still check their web
+        page though, using the <a target=outside
+        href="https://web.archive.org/web/20011128233927/http://www.plusv.org:80/" 
+        >Internet Archive</a>; and read the specification in <a target=outside
+        href="https://www.mp3-tech.org/programmer/docs/plusv.pdf">this PDF 
+        document</a> hosted by Gabriel Bouvigne.<br><br>
         
-        <i>Big thanks to Zachary Jelesoff for sending me these files and reminding me of 
-        the existence of PlusV.</i><br>
+        <i>Thanks to Zachary Jelesoff for sending me these files and reminding 
+        me of the existence of PlusV.</i><br>
 
         <br>
         <br>
