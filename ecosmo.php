fb6d2e232a9e8649abfa030d40bd89d3f5ca568c
Roberto Amorim <rjamorim@yahoo.com>
2024-01-28 13:57:10 -0300

    Turns out ECosmo devs are stealing from Xing. smh...

--- ecosmo.php
+++ ecosmo.php
@@ -54,9 +54,9 @@
     
     
       </table>
-<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+<br><br><br><br><br><br><br>
       <table border="0" cellpadding="0" cellspacing="0" width="120">
-    <tr><td height=250><img width=1 height=100 src="dot.gif"></td></tr>
+    <tr><td height=450><img width=1 height=100 src="dot.gif"></td></tr>
       </table>
       </font>
       </td>
@@ -99,11 +99,34 @@
         
         The encoder is distributed as part of a larger program called MPEG 
         Suite. It is quite featureful, offering batch encoding, transcoding and
-        decoding. Besides their own encoder - which seems to be little more than
-        a dist10 compile -, you can choose to encode using <a href=blade.php>
-        Blade</a> (version 0.76) and <a href=plugger.php>Plugger</a> (version 0.4)
-        which are distributed in the same installation. The decoding routines 
-        are based on <a href=xaudio.php>XAudio</a>.<br><br>
+        decoding. Besides their own encoder, you can choose to encode using <a 
+        href=blade.php>Blade</a> (version 0.76) and <a href=plugger.php>Plugger
+        </a> (version 0.4) which are distributed in the same installation. The 
+        decoding routines are based on <a href=xaudio.php>XAudio</a>.<br><br>
+        
+        Now, their encoder... in the documentation the authors claim it is super
+        fast, and that raises all kinds of red flags for me. So I started paying
+        attention to the encoding process, it is indeed super fast, but if you 
+        set the encoder to run on idle priority and increase the CPU load in
+        Windows, there is enough time to notice a process called codec.exe  
+        running on task manager. That executable appears on Electronic Cosmo's 
+        installation folder as soon as you start encoding, and disappears as 
+        soon as encoding is finished.<br><br>
+        
+        I copied the program (75kB) to another folder before it got deleted, 
+        and got to investigating. For starters, it is packed with ASpack - 
+        another red flag, but not a problem, I just unpacked it with AspackDie 
+        and generated a 360kB executable. Then I loaded it in an Hex editor and 
+        started looking for strings. The data starting at offset 204C8 is 
+        identical to the data starting at offset 1F0C8 in <a href=xing.php>Xing
+        tompg</a>, except for the strings precisely expliciting that we're
+        dealing with a Xing encoder.<br><br>
+        
+        Can it be a coincidence? Yes it can. But obviously isn't. Electronic 
+        Cosmo is <a href=rjpa.php>another</a> <a href=x-codec.php>encoder</a>
+        stealing from Xing and the developers are not even decent enough to 
+        give credit where it is due (at least rJPa acknowledges Xing in their 
+        program).<br><br>
         
         The web site is surprisingly still availabe, <a target=outside 
         href="http://ec2000.net/index.php">here</a>.<br><br>
