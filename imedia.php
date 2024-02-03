70211f06596a315fb53bbbe93302a9f01d5c4813
Roberto Amorim <rjamorim@yahoo.com>
2024-02-03 09:48:08 -0300

    Oops! Forgot the i-Media page

---
+++ imedia.php
@@ -0,0 +1,168 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+
+<HTML>
+<HEAD>
+<TITLE>ReallyRareWares - QDesign MPEG audio codecs</TITLE>
+
+</HEAD>
+<BODY bgcolor="#FFFFFF">
+
+<table border="0" cellpadding="2" cellspacing="0" width="100%">
+  <!-- Titulo -->
+  <tr>
+  <td>
+  <table border="0" cellpadding="0" cellspacing="0" width="100%">
+    <tr><td align="center"><img src="img/title.gif"></td>
+    </tr>
+  </table>
+  </td>
+  </tr>
+
+  <tr>
+  <td bgcolor="#6699CC"><img height=15 width=1 src="dot.gif" alt="null"></td>
+  </tr>
+
+  <tr>
+  <td>
+  <table border="0" cellpadding="0" cellspacing="0" width="100%">
+    <tr valign="top">
+    <!-- Menu -->
+    <td width=120 nowrap>
+    <table border="0" cellpadding="0" cellspacing="0">
+      <tr valign="top">
+      <td bgcolor="#999999" width=119 align="center" nowrap>
+      <font size="1" face="Tahoma">
+      <table border="0" cellpadding="0" cellspacing="0" width="120">
+    
+    </td>
+      </table>
+      <table border="0" cellpadding="0" cellspacing="0" width="120">
+    <tr><td bgcolor="#669999"><font face="Tahoma" size="2">
+      <body link="#999999" vlink="#999999" alink="#999999">
+        <center><img src="img/menu.gif"></center>
+    </font></td></tr>
+      </table>
+<br>
+    <?php include("menu.htm") ?>
+
+
+      <table border="0" cellpadding="0" cellspacing="0" width="120">
+    <tr><td></td></tr>
+    <tr></tr>
+    <tr><td></td></tr>
+    
+    
+      </table>
+<br><br><br><br><br><br><br><br>
+      <table border="0" cellpadding="0" cellspacing="0" width="120">
+    <tr><td height=450><img width=1 height=100 src="dot.gif"></td></tr>
+      </table>
+      </font>
+      </td>
+     
+      </tr>
+    </table></td>
+    <!-- Corpo principal -->
+    <td width="100%">
+    <table border="0" cellpadding="0" cellspacing="0" width="100%">
+      <tr><td>
+        <table border="0" cellpadding="0" cellspacing="0" width="100%">
+      <tr>
+      <td width="100%" align="right">
+      </td>
+      </tr>
+    </table></td></tr>
+      <tr><td>
+        <table border="0" cellpadding="10" cellspacing="0" width="100%">
+      <tr><td width="450"></td>
+        <td width="20%"></td></tr>
+      <tr><td>
+        <font face="Geneva,Verdana,Helvetica,Arial">
+
+        <!-- Corpo agora -->
+        
+        <table border="0" cellpadding="2" cellspacing="0" width="20">
+          <tr bgcolor="669999"><td width="20"><img width=20 height=1 src="dot.gif"></td>
+          <td><font face="Tahoma" size="4" color="black"><img src="img/programs2.gif"></font></td>
+          <td width="20"><img width=20 height=1 src="dot.gif"></td></tr>
+        </table>
+        </a>
+        <body link="#000000" vlink="#000000" alink="#000000">
+        <font face="Verdana" size=2 color="6699CC">
+          <br><div align="justify"><b> QDesign MPEG audio codecs </b></div><p></font>
+        <font face="Tahoma" size=2 color="black"><blockquote>
+        <a href="screens/i-Media.png" target=outside><img src="screens/i-Media.png" 
+        alt="i-Media MP2 Encoder 2.0" align=right height=240></a>
+        QDesign became a famous player in the audio encoding scene when they 
+        launched the <a href=qdmc.php>QDesign music codec</a>, as the advanced 
+        audio encoding option in QuickTime, in early 1998. But by then they had 
+        already built some fame for themselves as purveyors of quality MPEG 
+        audio encoders since 1996.<br><br>
+        
+        So here we have two codecs from QDesign, both provided by the great
+        Radium guys. i-Media is a graphical encoder and decoder for MP1 and MP2 
+        files, and one of its major features is encoding directly from line-in - 
+        a feature sought-after by broadcasters (which are the target audience, 
+        and that explains the steep license price: 495 dollars).<br><br>
+        
+        The encoder is also optimized to be fast, both in the high quality mode 
+        and even faster in the fast mode. The idea is to enable MP2 encoding in
+        real time from line-in on mid-1990s computers.<br><br>
+        
+        Then, we have an ACM codec for the Windows Audio Compression Manager. It
+        encodes only to MP2, but has the advantage of working with all <a 
+        href=acmenc.php>programs that</a> can <a href=acmstation.php>use ACM</a>.
+        Also, it comes with an interesting little app (that gets installed to the 
+        system tray) which allows users to change the playback speed of streams
+        using the respective decoder. And it was much cheaper than i-Media: "only"
+        79 dollars.<br><br>
+        
+        A third alternative for people wanting to enjoy the QDesign MP2 encoder - 
+        a free one! - is using the <a href=recordit.php>Iomega RecordIt</a> app. 
+        Sadly, it will not work with WAV files, but can rip from CDs without 
+        trouble.<br><br>
+        
+        QDesign de-emphasized their MPEG audio products in the early 2000s, 
+        focusing instead on QDMC and their music library manager <a href=qdmc.php
+        >MVP</a>. Then it got bought by DTS in 2004. You can see a mirror of their 
+        <a href="https://web.archive.org/web/19991001051347/http://www.qdesign.com/products/MPEGPro.htm"
+        target=outside>i-Media</a> page, as well as one of their <a target=outside
+        href="https://web.archive.org/web/19991001062437/http://www.qdesign.com/products/MPEG_ACM.htm"
+        >ACM codec</a> page, hosted at the Internet Archive.<br>
+
+        <br>
+        <br>
+        <a href="screens/mp2acm.png" target=outside><img src="screens/mp2acm.png" 
+        alt="QDesign MP2 ACM" align=right height=170></a>
+        Date: 1997-06-11<br>
+        Version: 2.0<br>
+        Interface: Graphical<br>
+        Platform: Win32<br>
+        Download: <a href="files/mpeg/imda200.zip">imda200.zip</a> - 1289kB<br>
+        <br>
+        Date: 1998-07-07<br>
+        Version: 1.01<br>
+        Interface: Graphical (ACM codec)<br>
+        Platform: Win32<br>
+        Download: <a href="files/mpeg/qdmp2acm.zip">qdmp2acm.zip</a> - 1588kB<br>
+
+        </blockquote></font>
+
+        <br><br>
+
+        </font>
+      </td></tr>
+    </table>
+      </td></tr>
+    </table>
+    </td>
+    </tr>
+  </table></td>
+  </tr>
+</table> 
+ 
+<?php include("footer.htm") ?>
+
+</body>
+</html>
