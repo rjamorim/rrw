94f1fdf506ab9c920e73460d16e6cb33a5652678
Roberto Amorim <rjamorim@yahoo.com>
2024-08-02 14:41:13 -0300

    Added descriptions of the programs featured in the page

--- nttvqf.php
+++ nttvqf.php
@@ -54,9 +54,9 @@
     
     
       </table>
-<br><br><br><br><br><br><br>
+<br><br><br><br><br><br>
       <table border="0" cellpadding="0" cellspacing="0" width="120">
-    <tr><td height=600><img width=1 height=100 src="dot.gif"></td></tr>
+    <tr><td height=700><img width=1 height=100 src="dot.gif"></td></tr>
       </table>
       </font>
       </td>
@@ -93,35 +93,49 @@
           <br><div align="justify"> <b>NTT TwinVQ encoder</b></div> <p>
         </font>
         <font face="Tahoma" size=2 color="black"><blockquote>
-        <a href="screens/nttvqf-ply.png" target=outside><img src="screens/nttvqf-ply.png" 
+        <a href="screens/nttvqf-ply.png" target=_blank><img src="screens/nttvqf-ply.png" 
         alt="TwinVQ Player" align=right></a>
-        TwinVQ is an audio encoding technology developed at NTT (Nippon Telephone and
-        Telegraph). It uses an unique encoding technique called Vector Quantization 
-        (VQ).<br><br>
+        TwinVQ is an audio encoding technology developed at NTT (Nippon Telephone 
+        and Telegraph, the developers of <a href=dualspeech.php>DualSpeech</a>).
+        It uses an unique encoding technique called Vector Quantization (VQ).
+        <br><br>
         
-        The format using the TwinVQ technology is called VQF. There are several 
+        VQF is the format created for the TwinVQ technology. There are several 
         limitations in this format: 2 channels at most, no VBR, limited bitrate 
-        choice (only 80 and 96kbps for 44.1kHz stereo tracks), very slow encoding,
-        little support, no public specifications or open source implementation.<br><br>
+        options (only 80 and 96kbps for 44.1kHz stereo tracks), slow encoding,
+        little support, no public specifications or open source implementation.
+        <br><br>
         
-        Despite these limitations, Yamaha bought the technology from NTT and launched
-        the <a href=soundvq.php>SoundVQ</a> suite. But it did not work out and VQF 
-        can safely be considered dead these days.<br><br>
+        Despite these limitations, Yamaha bought the technology from NTT and 
+        launched the <a href=soundvq.php>SoundVQ</a> suite based on it. But it 
+        did not work out and VQF can be safely considered dead these days.<br><br>
         
-        TwinVQ, on the other hand, is still alive as part of the MPEG4 audio standard,
-        being used for very small bitrates (8-16kbps).<br><br>
+        TwinVQ, on the other hand, is still alive as part of the MPEG4 audio 
+        standard, being used for very low bitrates (8-16kbps).<br><br>
         
-        A mirror of the NTT TwinVQ page is available at the <a 
+        The player available here is very basic. It only supports VQF files and
+        VQL playlists (but, for some reason, refused the VQL playlist I generated
+        with the Yamaha encoder). The only controls are play, pause, stop, repeat
+        and volume. The encoder is also as simple as can get: no batch support, 
+        only tags available are "Title" and "Comment", just a handful of sampling
+        rates supported. Both give off a feeling of being demonstration apps 
+        rather than actual commercial products.<br><br>
+        
+        At the bottom of the page you'll also find the NTT TwinVQ SDK, with 
+        precompiled libraries for encoding and decoding on Windows and Linux.
+        <br><br>
+        
+        A mirror of the NTT TwinVQ page is available at the <a target=_blank
         href="https://web.archive.org/web/20041012071040/http://www.twinvq.org/english/index_en.html" 
-        target=outside>Internet Archive</a>.<br><br>
+        >Internet Archive</a>.<br><br>
         
         I am also mirroring Thor's <a href="mirrors/vqf/index.html">page</a> from 
         dalnetvqf, featuring VQF plugins for Winamp. Thanks to Thor for providing 
-        these files.<br>
+        these files!<br>
         
         <br>
         <br>
-        <a href="screens/nttvqf-enc.png" target=outside><img src="screens/nttvqf-enc.png" 
+        <a href="screens/nttvqf-enc.png" target=_blank><img src="screens/nttvqf-enc.png" 
         alt="TwinVQ Encoder" align=right height=238></a>
         Date: 1998-03-11<br>
         Version: 2.1.1 (encoder)<br>
@@ -142,7 +156,7 @@
         Download: <a href="files/vqf/in_vqf.exe">in_vqf.exe</a> - 419kB<br>
         <br>
         Date: 2000-10-12<br>
-        Version: 20001012<br>
+        Version: 2.3<br>
         Interface: SDK<br>
         Platform: Win32/Linux<br>
         Download: <a href="files/vqf/Tvq20001012_open_nc.zip">Tvq20001012_open_nc.zip</a> - 3.152kB<br>
