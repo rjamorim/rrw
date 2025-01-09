6b50b487ec3b5fd292b72a37c9d4536783c8ad9e
Roberto Amorim <rjamorim@yahoo.com>
2025-01-09 14:22:29 -0300

    A bunch of improvements

--- macplay3.php
+++ macplay3.php
@@ -54,9 +54,9 @@
     
     
       </table>
-<br><br><br><br><br><br><br>
+<br><br><br><br><br><br><br><br><br>
       <table border="0" cellpadding="0" cellspacing="0" width="120">
-    <tr><td height=400><img width=1 height=100 src="dot.gif"></td></tr>
+    <tr><td height=450><img width=1 height=100 src="dot.gif"></td></tr>
       </table>
       </font>
       </td>
@@ -92,30 +92,31 @@
         <font face="Verdana" size=2 color="6699CC">
           <br><div align="justify"> <b>FhG IIS MacPlay3</b></div> <p> </font>
         <font face="Tahoma" size=2 color="black"><blockquote>
-        <a href="screens/macplay3v14.png" target=outside><img src="screens/macplay3v14.png" 
+        <a href="screens/macplay3v14.png" target=_blank><img src="screens/macplay3v14.png" 
         alt="MacPlay3 v1.4" align=right></a>
-        MacPlay3 was Fraunhofer's port of the <a href="winplay3.php">WinPlay3</a> 
-        MP3 player for Mac OS Classic. It has the same (few) features of its Windows 
-        counterpart.<br><br>
+        MacPlay3 was Fraunhofer's port of the <a href=winplay3.php>WinPlay3</a> 
+        MP3 player for Mac OS Classic. It has the same (few) features of its 
+        Windows counterpart.<br><br>
         
         The alpha 0.99 version works on the M68K platform, but it is only useful 
         for decoding, since the 68k CPU is not powerful enough to playback MP3 in 
-        real time. Besides, it is a demo version with no interface (all commands 
+        real-time. Besides, it is a demo version with no interface (all commands 
         are done through the menu bar, similar to Winamp 0.2) and playback is 
         limited to 20 seconds (decoding to AIFF is not limited).<br><br>
         
         Version 1.4b2 was the last version released by FhG and only works on 
-        PowerPC machines, but it has an actual interface and no limitations.<br><br>
+        PowerPC machines, but it has an actual interface and no limitations.
+        <br><br>
         
-        A mirror of the MacPlay3 website is available at the <a 
+        A mirror of the MacPlay3 website is available at the <a target=_blank
         href="https://web.archive.org/web/19980204231239/http://iis.fhg.de:80/departs/amm/layer3/macplay3/index.html"
-        target=outside>Internet Archive</a>. <br><br>
+        >Internet Archive</a>.<br><br>
         
-        At least, version 0.99 had the very best installation blurb...<br>
+        At least version 0.99 had the very best installation blurb...<br>
         
         <br>
         <br>
-        <a href="screens/macplay3v099-inst.png" target=outside><img src="screens/macplay3v099-inst.png" 
+        <a href="screens/macplay3v099-inst.png" target=_blank><img src="screens/macplay3v099-inst.png" 
         alt="MacPlay3 v0.99 installation" align=right height=180></a>
         Date: 1997-05-27<br>
         Version: 1.4b2<br>
